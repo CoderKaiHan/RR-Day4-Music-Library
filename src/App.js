44301c349e06268b1f5bd804934d1c6bd3193d11
CoderKaiHan <hankaicoder@gmail.com>
2024-02-26 11:46:31 -0500

    Implement Reac Suspense

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
+import { createResource as fetchData } from './helper'
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+import Spinner from './components/Spinner';
 
 function App(){
-    let [search, setSearch] = useState('');
+    let [searchTerm, setSearchTerm] = useState('');
     let [message, setMessage] = useState('Search for Music!');
-    let [data, setData] = useState([]);
-
-    const API_URL = 'https://itunes.apple.com/search?term=';
+    let [data, setData] = useState(null);
 
     useEffect(() =>{
-      if (search){
-          const fetchData = async () => {
-            document.title = `${search} Music`;
-            const response = await fetch (API_URL + search);
-            const resData = await response.json();
-            console.log(resData);
-            if(resData.results.length > 0){
-              setData(resData.results);
-            } else {
-              setMessage('Not Found');
-            }
+      if (searchTerm){
+        setData(fetchData(searchTerm))
+          // const fetchData = async () => {
+          //   document.title = `${search} Music`;
+          //   const response = await fetch (API_URL + search);
+          //   const resData = await response.json();
+          //   console.log(resData);
+          //   if(resData.results.length > 0){
+          //     setData(resData.results);
+          //   } else {
+          //     setMessage('Not Found');
+          //   }
           }
-          fetchData();
-      }
-    },[search]);
+          // fetchData();
+      },[searchTerm]);
     
     const handleSearch = (e, term) => {
       e.preventDefault();
-      setSearch(term);
+      setSearchTerm(term);
+    }
+    
+    const renderGallery = () =>{
+      if(data) {
+        return (
+          <Suspense fallback={<Spinner />}>
+            <Gallery data={data}/>
+          </Suspense>
+        );
+      }
     }
 
     return (
         <div>
             <SearchBar handleSearch = {handleSearch}/>
             {message}
-            <Gallery data={data}/>
+            {renderGallery()}
         </div>
     )
 }
